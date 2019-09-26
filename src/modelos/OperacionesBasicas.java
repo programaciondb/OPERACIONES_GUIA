ea4faa8a90039865082fe8c9334e7ed553c56ad9
David Bousquet <david@programaciondb.cl>
2019-09-26 16:21:26 -0300

    Se implementan métodos de clase Operaciones Basicas

--- src/modelos/OperacionesBasicas.java
+++ src/modelos/OperacionesBasicas.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelos;
 
-/**
- *
- * @author davidbousquet
- */
 public class OperacionesBasicas {
-    
+
+    public int Sumar(int a, int b) {
+        if (a != 0 & b != 0) {
+            return a + b;
+        }
+        return 0;
+    }
+
+    public int Restar(int a, int b) {
+        if (a >= b) {
+            return a - b;
+        }
+        return 0;
+    }
+
+    public int Multiplicar(int a, int b) {
+        if (b != 0) {
+            return a * b;
+        }
+        return 1;
+    }
+
+    public int Dividir(int a, int b) {
+        if (b != 0) {
+            return a / b;
+        }
+        return 0;
+    }
+
 }
