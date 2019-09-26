e34e957d59cba30db396eb119f00b722c2b7fed4
David Bousquet <david@programaciondb.cl>
2019-09-26 16:24:49 -0300

    Se crea archivos base para realizar los test, sin contenido

--- test/modelos/AllTest.java
+++ test/modelos/AllTest.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelos;
 
-/**
- *
- * @author davidbousquet
- */
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    TestSumar.class, TestRestar.class, TestMultiplicar.class, TestDividir.class
+})
 public class AllTest {
-    
+
 }
